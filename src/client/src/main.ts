a8288938bebfd15d4e89544f0a86cd3c8db7a774
H0rn0chse <h0rn0chse@gmx.de>
2023-11-19 12:29:53 +0100

    Added i18n

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from "./App.vue";
-import router from "./router";
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
+
+import i18nConfig from "./lang/index";
+
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(createVuestic());
+app.use(i18nConfig);
+
 app.mount("body");
--- src/client/src/views/HomeView.vue
+++ src/client/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 </script>
 
 <template>
   <main>
     <va-button color="info" class="mr-6 mb-2">
-      Info
+      {{ t("test") }}
     </va-button>
   </main>
 </template>
