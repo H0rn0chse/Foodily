c007b2bc3be38b97327e54a749ea3819770bb70d
H0rn0chse <h0rn0chse@gmx.de>
2023-12-07 08:38:36 +0100

    Updated env and Login

--- src/app-public/src/login/views/LoginView.vue
+++ src/app-public/src/login/views/LoginView.vue
@@ -1,38 +1,42 @@
 <script setup lang="ts">
-// import { useForm } from "vuestic-ui";
-// import { useI18n } from "vue-i18n";
+import { useI18n } from "vue-i18n";
 import { reactive } from "vue";
-// const { t } = useI18n();
+const { t } = useI18n();
 
-// const form = reactive({
-//   login: "",
-//   password: "",
-// });
+const form = reactive({
+  login: "",
+  password: "",
+});
 
-// const { errorMessagesNamed } = useForm("myForm");
+// function handleSubmit() {
+//   alert("hi");
+// }
 </script>
 
 <template>
   <main>
     <h1>Sign in</h1>
-    <!-- <VaForm ref="myForm"
-      immediate
-      hide-error-messages
-      class="flex flex-col gap-2 mb-2">
+    <VaForm id="login"
+      class="flex flex-col gap-2 mb-2"
+      tag="form"
+      action="/login/password"
+      method="post">
       <VaInput v-model="form.login"
-        label="Login"
-        name="Login"
-        :rules="[(v) => Boolean(v) || 'Login is required']" />
-      <VaInput v-model="form.password"
-        label="Password"
-        type="password"
-        name="Password"
+        :label="t('login.username')"
+        name="username"
         :rules="[
-          (v) => Boolean(v) || 'Password is required',
-          (v) => v.length > 6 || 'Password must be a least 6 symbols',
+          (v) => Boolean(v) || t('login.username.empty'),
         ]" />
-    </VaForm> -->
-    <form action="/login/password"
+      <VaInput v-model="form.password"
+        :label="t('login.password')"
+        name="password"
+        type="password"
+        :rules="[
+          (v) => Boolean(v) || t('login.password.empty'),
+        ]" />
+      <VaButton type="submit">Sign In </VaButton>
+    </VaForm>
+    <!-- <form action="/login/password"
       method="post">
       <section>
         <label for="username">Username</label>
@@ -51,15 +55,21 @@ import { reactive } from "vue";
           autocomplete="current-password"
           required>
       </section>
-      <VaButton type="submit">Sign In </VaButton>
-      <!-- <button type="submit">Sign in</button> -->
-    </form>
+      <VaButton type="submit">Sign In </VaButton> -->
+    <!-- <button type="submit">Sign in</button> -->
+    <!-- </form> -->
   </main>
 </template>
 
 <style scoped>
 h1 {
   font-size: xx-large;
+}
+
+#login {
+  max-width: 20rem;
+  display: flex;
+  flex-direction: column;
 }
 
 a {
